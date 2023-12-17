72c9e4e73630dae3ce345f917852262b947df54c
Zhehao <x97589125@gmail.com>
2023-12-17 00:25:41 -0800

    test

--- Wurq/app/models/DataStore.ts
+++ Wurq/app/models/DataStore.ts
@@ -1,32 +1,32 @@
 import { observable, action } from 'mobx';
 
 interface ChartDataset {
-    data: number[];
+  data: number[];
 }
 
 interface ExerciseData {
-    
-  }
 
-  interface ChartData {
-    labels: number[];
-    datasets: ChartDataset[];
-  }
-  
-  interface DataStoreState {
-    chartData: ChartData;
-    date_time: string;
-    name: string;
-    time: number;
-    rest: number;
-    hr: number;
-    points: number;
-    exercises: string;
-  }
+}
+
+interface ChartData {
+  labels: number[];
+  datasets: ChartDataset[];
+}
+
+interface DataStoreState {
+  chartData: ChartData;
+  date_time: string;
+  name: string;
+  time: number;
+  rest: number;
+  hr: number;
+  points: number;
+  exercises: string;
+}
 
 class DataStore {
- @observable data: DataStoreState = {
-    chartData: {labels:[], datasets:[]},
+  @observable data: DataStoreState = {
+    chartData: { labels: [], datasets: [] },
     date_time: "",
     name: "",
     time: 0,
@@ -39,7 +39,7 @@ class DataStore {
   @action updateData(newData) {
     const n = newData.points_per_wod.length
     this.data.chartData.labels = Array.from({ length: n }, (_, index) => index + 1);
-    this.data.chartData.datasets = [{data:newData.points_per_wod}]
+    this.data.chartData.datasets = [{ data: newData.points_per_wod }]
     this.data.date_time = newData.history[0].date_time
     this.data.name = newData.history[0].name
     this.data.time = newData.history[0].time
@@ -49,11 +49,11 @@ class DataStore {
     this.data.exercises = newData.history[0].exercises
   }
 
-  @action updateName(name:string) {
+  @action updateName(name: string) {
     this.data.name = name;
   }
-  @action updatePoints(points:number) {
-    this.data.points =points;
+  @action updatePoints(points: number) {
+    this.data.points = points;
   }
 }
 
